29dceea8f4ac601573f21c006d736cad425ed242
Marco Ravelo <mravelo5874@gmail.com>
2023-02-09 00:05:56 -0600

    trying to get to work :(

--- Project 2/p2-distrib/transformer.py
+++ Project 2/p2-distrib/transformer.py
@@ -1,5 +1,6 @@
 # transformer.py
 
+import math
 import time
 import torch
 import torch.nn as nn
@@ -33,7 +34,7 @@ class LetterCountingExample(object):
 # a single layer of the Transformer; this Module will take the raw words as input and do all of the steps necessary
 # to return distributions over the labels (0, 1, or 2).
 class Transformer(nn.Module):
-    def __init__(self, vocab_size, num_positions, d_model, d_ff, num_classes, num_layers):
+    def __init__(self, num_pos, d_model, num_heads, d_ff, num_classes, num_layers, dropout):
         """
         :param vocab_size: vocabulary size of the embedding layer
         :param num_positions: max sequence length that will be fed to the model; should be 20
@@ -43,40 +44,47 @@ class Transformer(nn.Module):
         :param num_layers: number of TransformerLayers to use; can be whatever you want
         """        
         super().__init__()
-        self.pos_encod = PositionalEncoding(d_model, num_positions, batched=True)
-        self.trans_layers = nn.ModuleList([my_transformer_layer(d_model, d_ff) for _ in range(num_layers)])
+        self.pos_encode = PositionalEncoding(d_model, num_pos, True)
+        self.trans_layers = nn.ModuleList([my_transformer_layer(d_model, num_heads, d_ff, dropout) for _ in range(num_layers)])
         self.linear = nn.Linear(d_model, num_classes)
-        self.softmax = nn.Softmax(dim=2)
-
-    def forward(self, x):
+        self.softmax = nn.LogSoftmax(dim=2)
+        
+    def get_atten_maps(self):
+        maps_list = []
+        for layer in self.trans_layers:
+            maps_list.append(layer.get_atten_maps())
+        return torch.cat(maps_list)
+        
+    def forward(self, x) -> torch.Tensor:
         """
         :param indices: list of input indices
         :return: A tuple of the softmax log probabilities (should be a 20x3 matrix) and a list of the attention
         maps you use in your layers (can be variable length, but each should be a 20x20 matrix)
         """
-        # (1) adding positional encodings to the input (see the PositionalEncoding class; but we recommend leaving these out for now)
         #print ('x.shape init: ', x.shape)
-        x = torch.unsqueeze(x, dim=2)
-        #print ('x.shape squeeze: ', x.shape)
-        x = self.pos_encod(x) # input -> [batch size, seq len, embedding dim]
+        # (0) add extra dimension for embedding
+        x = torch.unsqueeze(x, dim=len(x.shape))
+        #print ('x.shape unsqueeze: ', x.shape)
+        # (1) adding positional encodings to the input (see the PositionalEncoding class; but we recommend leaving these out for now)
+        x = self.pos_encode(x) # input -> [batch size, seq len, embedding dim]
         #print ('x.shape pos encode: ', x.shape)
         # (2) using one or more of your TransformerLayers;
-        x = torch.cat([trans(x) for trans in self.trans_layers])
+        for layer in self.trans_layers:
+            x = layer(x)
         #print ('x.shape trans: ', x.shape)
-        
         # (3) using Linear and softmax layers to make the prediction
         x = self.linear(x)
         #print ('x.shape linear: ', x.shape)
         x = self.softmax(x)
-        #print ('x.shape softmax: ', x.shape)
-        return x
-
+        #print ('x.shape softmax: ', x.shape)    
+        x = torch.squeeze(x)
+        return x, self.get_atten_maps()
 
 # Your implementation of the Transformer layer goes here. It should take vectors and return the same number of vectors
 # of the same length, applying self-attention, the feedforward layer, etc.
 # used https://medium.com/the-dl/transformers-from-scratch-in-pytorch-8777e346ca51 by Frank Odom to help my implementation :)
 class my_transformer_layer(nn.Module):
-    def __init__(self, d_model, d_ff, heads=1, dropout=0.1):
+    def __init__(self, d_model: int, num_heads: int, d_ff: int, dropout: float):
         """
         :param d_model: The dimension of the inputs and outputs of the layer (note that the inputs and outputs
         have to be the same size for the residual connection to work)
@@ -84,19 +92,98 @@ class my_transformer_layer(nn.Module):
         should both be of this length.
         """
         super().__init__()
-        d_k = max(d_model // heads, 1)
         self.atten = my_residual(
-            my_multi_head_atten(heads, d_model, d_k),
-            dim=d_model,
-            dropout=dropout)
+            my_multi_head_atten(num_heads, d_model),
+            d_model,
+            dropout,
+        )
         self.ff = my_residual(
             my_ff(d_model, d_ff),
-            dim=d_model,
-            dropout=dropout
+            d_model,
+            dropout,
         )
+        
+    def get_atten_maps(self):
+        return self.atten.sublayer.get_atten_maps()
+        
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        x = self.atten(x, x, x)
+        x = self.ff(x)
+        return x
 
-    def forward(self, input_vecs: torch.Tensor) -> torch.Tensor:
-        x = self.atten(input_vecs, input_vecs, input_vecs)
+# class my_transformer_decoder(nn.Module):
+#     def __init__(self, num_layers: int, num_pos: int, d_model: int, num_heads: int, d_ff: int, dropout: float):
+#         super().__init__()
+#         self.pos_encode = PositionalEncoding(d_model, num_pos, batched=True)
+#         self.layers = nn.ModuleList(
+#             my_transformer_decoder_layer(d_model, num_heads, d_ff, dropout)
+#             for _ in range(num_layers)
+#         )
+#         self.linear = nn.Linear(d_model, d_model)
+        
+#     def forward(self, tgt: torch.Tensor, mem: torch.Tensor) -> torch.Tensor:
+#         tgt = self.pos_encode(tgt)
+#         for layer in self.layers:
+#             tgt = layer(tgt, mem)
+#         tgt = torch.softmax(self.linear(tgt), dim=-1)
+#         return tgt
+
+# class my_transformer_decoder_layer(nn.Module):
+#     def __init__(self, d_model: int, num_heads: int, d_ff: int, dropout: float):
+#         super().__init__()
+#         self.atten_1 = my_residual(
+#             my_multi_head_atten(num_heads, d_model),
+#             d_model,
+#             dropout,
+#         )
+#         self.atten_2 = my_residual(
+#             my_multi_head_atten(num_heads, d_model),
+#             d_model,
+#             dropout,
+#         )
+#         self.ff = my_residual(
+#             my_ff(d_model, d_ff),
+#             d_model,
+#             dropout,
+#         )
+        
+#     def forward(self, tgt: torch.Tensor, mem: torch.Tensor) -> torch.Tensor:
+#         tgt = self.atten_1(tgt, tgt, tgt)
+#         tgt = self.atten_2(tgt, mem, mem)
+#         tgt = self.ff(tgt)
+#         return tgt
+
+# class my_transformer_encoder(nn.Module):
+#     def __init__(self, num_layers: int, num_pos: int, d_model: int, num_heads: int, d_ff: int, dropout: float):
+#         super().__init__()
+#         self.pos_encode = PositionalEncoding(d_model, num_pos, batched=True)
+#         self.layers = nn.ModuleList(
+#             my_transformer_encoder_layer(d_model, num_heads, d_ff, dropout)
+#             for _ in range(num_layers)
+#         )
+        
+#     def forward(self, x: torch.Tensor) -> torch.Tensor:
+#         x = self.pos_encode(x)
+#         for layer in self.layers:
+#             x = layer(x)
+#         return x
+        
+class my_transformer_encoder_layer(nn.Module):
+    def __init__(self, d_model: int, num_heads: int, d_ff: int, dropout: float):
+        super().__init__()
+        self.atten = my_residual(
+            my_multi_head_atten(num_heads, d_model),
+            d_model,
+            dropout,
+        )
+        self.ff = my_residual(
+            my_ff(d_model, d_ff),
+            d_model,
+            dropout,
+        )
+        
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        x = self.atten(x, x, x)
         x = self.ff(x)
         return x
 
@@ -113,40 +200,55 @@ class my_residual(nn.Module):
         return self.norm(tensors[0] + self.drop(self.sublayer(*tensors)))
     
 # implements a feed-forward module that is used by the residual layer in the transformer layer
-def my_ff(dim_in: int, dim_ff: int) -> nn.Module:
+def my_ff(d_model: int, d_ff: int) -> nn.Module:
     return nn.Sequential(
-        nn.Linear(dim_in, dim_ff),
+        nn.Linear(d_model, d_ff),
         nn.ReLU(),
-        nn.Linear(dim_ff, dim_in)
+        nn.Linear(d_ff, d_model)
     )
 
 # implements multiple attention heads
 class my_multi_head_atten(nn.Module):
-    def __init__(self, num_heads: int, dim_in: int, d_k: int):
+    def __init__(self, num_heads: int, d_model: int):
         super().__init__()
-        self.heads = nn.ModuleList([my_attention_head(dim_in, d_k, d_k) for _ in range(num_heads)])
-        self.linear = nn.Linear(num_heads * d_k, dim_in)
+        d_k = d_model // num_heads
+        self.heads = nn.ModuleList([my_attention_head(d_model, d_k) for _ in range(num_heads)])
+        self.linear = nn.Linear(num_heads * d_k, d_model)
+        self.atten = None
+        
+    def get_atten_maps(self):
+        maps_list = []
+        for h in self.heads:
+            maps_list.append(h.get_atten_map())
+        return torch.cat(maps_list)
     
     def forward(self, _q: torch.Tensor, _k: torch.Tensor, _v: torch.Tensor) -> torch.Tensor:
-        return self.linear(torch.cat([head(_q, _k, _v) for head in self.heads], dim=-1))
-
+        return self.linear(torch.cat([h(_q, _k, _v) for h in self.heads], dim=-1))
+    
 # implements a single attention head
 class my_attention_head(nn.Module):
-    def __init__(self, dim_in: int, dim_q: int, dim_k: int):
+    def __init__(self, d_model: int, d_k: int):
         super().__init__()
-        self.q = nn.Linear(dim_in, dim_q)
-        self.k = nn.Linear(dim_in, dim_k)
-        self.v = nn.Linear(dim_in, dim_k)
+        self.d_k = d_k
+        self.q = nn.Linear(d_model, d_k)
+        self.k = nn.Linear(d_model, d_k)
+        self.v = nn.Linear(d_model, d_k)
+        self.atten_map = None
+    
+    def get_atten_map(self):
+        return self.atten_map
         
     def forward(self, _q: torch.Tensor, _k: torch.Tensor, _v: torch.Tensor) -> torch.Tensor:
-        return scaled_dot_product_attention(self.q(_q), self.k(_k), self.v(_v))
+        res, atten_map = scaled_dot_product_attention(self.q(_q), self.k(_k), self.v(_v), self.d_k)
+        self.atten_map = atten_map
+        return res
     
 # used for forward() of single attention head
-def scaled_dot_product_attention(q: torch.Tensor, k: torch.Tensor, v: torch.Tensor):
-    numerator = q.bmm(k.transpose(1, 2))
-    scale = q.size(-1) ** 0.5
-    sm = func.softmax(numerator / scale, dim=-1)
-    return sm.bmm(v)
+def scaled_dot_product_attention(q: torch.Tensor, k: torch.Tensor, v: torch.Tensor, d_k: int):
+    scores = q.bmm(k.transpose(1, 2)) / math.sqrt(d_k)
+    atten_map = func.softmax(scores, dim=-1)
+    output = atten_map.bmm(v)
+    return output, atten_map
 
 
 # Implementation of positional encoding that you can use in your network
@@ -183,27 +285,33 @@ class PositionalEncoding(nn.Module):
 
 class MY_DATASET(Dataset):
     
-    def __init__(self, features, labels):
+    def __init__(self, features, labels, chars):
         self.features = features
         self.labels = labels
+        self.chars = chars
+        
+    def get_chars(self):
+        return self.chars
         
     def __len__(self):
-            return len(self.labels)
+        return len(self.labels)
         
     def __getitem__(self, idx):
-            feature = self.features[idx]
-            label = self.labels[idx]
-            return feature, label
+        feature = self.features[idx]
+        label = self.labels[idx]
+        return feature, label, self.chars[idx]
         
 def set_up_dataloader(train: List[LetterCountingExample], batch_size: int) -> DataLoader:
     input_list = []
     output_list = []
+    chars_list = []
     # put example inputs and outputs into lists
     for ex in train:
         input_list.append(ex.input_tensor)
         output_list.append(ex.output_tensor)
+        chars_list.append(ex.input)
     # create dataset and dataloader
-    dataset = MY_DATASET(input_list, output_list)
+    dataset = MY_DATASET(input_list, output_list, chars_list)
     dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True, drop_last=True)
     return dataloader
 
@@ -215,31 +323,44 @@ def one_hot_y(_y: torch.Tensor, batch_size: int) -> torch.Tensor:
     y_oh = torch.FloatTensor(y_oh)
     return y_oh
 
+def plot_maps(attn_maps, chars, plot_first=True):
+    for j in range(0, len(attn_maps)):
+        attn_map = attn_maps[j]
+        fig, ax = plt.subplots()
+        im = ax.imshow(attn_map.detach().numpy(), cmap='hot', interpolation='nearest')
+        ax.set_xticks(np.arange(len(chars[j])), labels=chars[j])
+        ax.set_yticks(np.arange(len(chars[j])), labels=chars[j])
+        ax.xaxis.tick_top()
+        plt.show()
+        if (plot_first): return
+
 # This is a skeleton for train_classifier: you can implement this however you want
 def train_classifier(args, train, dev):
     # set up dataloader for batching
-    batch_size = 8
+    batch_size = 64
     train_dataloader = set_up_dataloader(train, batch_size)
     # create model
-    model = Transformer(vocab_size=27, num_positions=20, d_model=512, d_ff=2048, num_classes=3, num_layers=1)
+    model = Transformer(num_pos=20, d_model=512, num_heads=1, d_ff=2048, num_classes=3, num_layers=1, dropout=0.1)
     model.zero_grad()
     model.train()
     optimizer = optim.Adam(model.parameters(), lr=1e-4)
     
     # train model
-    num_epochs = 5
+    num_epochs = 3
     print ('[starting training]')
     for epoch in range(0, num_epochs):
         loss_this_epoch = 0.0
         random.seed(epoch)
         # You can use batching if you'd like
-        loss_func = nn.BCELoss()
-        for x, y in tqdm(train_dataloader):
+        loss_func = nn.BCEWithLogitsLoss()
+        for x, y, c in tqdm(train_dataloader):
             # restructure y to be [batch_size, 20, 3]
             y = one_hot_y(y, batch_size)
             
             # forward pass
-            p = model.forward(x)
+            p, maps = model.forward(x)
+            #print ('p.shape: ', p.shape)
+            #plot_maps(maps, c)
 
             # fix pred and y tensors for NLLLoss
             p = torch.reshape(p, (batch_size * 20, 3))
